6661c2144710294f7b679edd1efeb2b6d78dec17
Martyn Gigg <martyn.gigg@gmail.com>
2015-07-06 17:06:35 +0100

    Clear warnings around signal handler

--- cpp/qt/embed_python/main.cpp
+++ cpp/qt/embed_python/main.cpp
@@ -6,27 +6,29 @@
 /* signal example */
 #include <csignal>     /* signal, raise, sig_atomic_t */
 #include <execinfo.h>     /* signal, raise, sig_atomic_t */
+#include <iostream>
 
-void custom_signal_handler(int sig) {
+/**
+ * Print a backtrace to stderr when a SIGSEGV is received
+ */
+void custom_signal_handler(int) {
   QStringList stacktrace;
   stacktrace.append("\n********* SEGMENTATION FAULT *********\n"
                     "BACKTRACE:\n");
   void *trace[32];
-  size_t size = backtrace( trace, 32 );
-  char **strings = backtrace_symbols( trace, size );
-  for( size_t i = 0; i < size; i++ ){
+  int size = backtrace(trace,32);
+  char **strings = backtrace_symbols(trace, size);
+  for( int i = 0; i < size; i++ ){
     stacktrace.append(strings[i]);
   }
   stacktrace.append("\n**************************************\n\n");
-  QMessageBox::critical(NULL, "Python Interpreter",
-                        stacktrace.join("\n"));
+  std::cerr << stacktrace.join("\n").toStdString();
   exit(-1);
 }
 
 int main(int argc, char *argv[])
 {
-  void (*prev_handler)(int);
-  prev_handler = signal (SIGSEGV, custom_signal_handler);
+  signal (SIGSEGV, custom_signal_handler);
 
   QApplication app(argc, argv);
   app.setOrganizationName("GlobalInc");
