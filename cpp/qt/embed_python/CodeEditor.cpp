12c0b907053f0b83009bafc318e01f449e007443
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-10-22 14:55:13 +0100

    Add changes to codeeditor

--- cpp/qt/embed_python/CodeEditor.cpp
+++ cpp/qt/embed_python/CodeEditor.cpp
@@ -1,8 +1,40 @@
-#include "CodeEditor.h"
-#include <QVBoxLayout>
+#include "codeeditor.h"
+#include "ui_codeeditor.h"
 
-CodeEditor::CodeEditor(QWidget *parent) : QWidget(parent), m_editor(new QTabWidget(this)) {
-  this->setLayout(new QVBoxLayout);
-  layout()->addWidget(m_editor);
-  this->setFocusProxy(m_editor);
+#include <QPlainTextEdit>
+
+/**
+ *
+ * @param parent
+ */
+CodeEditor::CodeEditor(QWidget *parent) :
+  QWidget(parent),
+  ui(new Ui::CodeEditor)
+{
+  ui->setupUi(this);
+
+  ui->tabWidget->addTab(new QPlainTextEdit, "Tab 1");
+  ui->tabWidget->addTab(new QPlainTextEdit, "Tab 2");
+  
+  ui->tabWidget->setCurrentIndex(0);
+  
+  connect(ui->execButton, SIGNAL(clicked()), this, SIGNAL(goClicked()));
+}
+
+/**
+ *
+ */
+CodeEditor::~CodeEditor()
+{
+  delete ui;
+}
+
+/**
+ *
+ * @return A Code object with the text from the active tab
+ */
+Code CodeEditor::sourceCode() const
+{
+  QWidget* widget = ui->tabWidget->currentWidget();
+  return Code(QString(qobject_cast<QPlainTextEdit*>(widget)->toPlainText()));
 }
