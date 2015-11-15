9f9e2e8067e45d20894f7984c797030b1ef986d3
Martyn Gigg <martyn.gigg@gmail.com>
2015-11-15 15:14:45 +0000

    Add example of using a Python descriptor

---
+++ python/descriptors.py
@@ -0,0 +1,30 @@
+class TypedProperty(object):
+
+    def __init__(self, name, type, default=None):
+        self.name = "_" + name
+        self.type = type
+        self.default = default if default else type()
+
+    def __get__(self, instance, cls):
+        return getattr(instance, self.name, self.default)
+
+    def __set__(self,instance,value):
+        if not isinstance(value,self.type):
+            raise TypeError("Must be a %s" % self.type)
+        setattr(instance,self.name,value)
+
+    def __delete__(self,instance):
+        raise AttributeError("Can't delete attribute")
+
+
+class FooClassAttrs(object):
+    name = TypedProperty("name",str)
+    num = TypedProperty("num",int,42)
+
+# ------------------------------------------------------
+
+acct = FooClassAttrs()
+acct.name = "obi"
+acct.num = 1234
+print acct.num
+print acct.name
