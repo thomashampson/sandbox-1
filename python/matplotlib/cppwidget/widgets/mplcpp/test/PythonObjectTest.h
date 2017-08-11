d47675eea6fb213ffd974c043be7fd60717f88ba
Martyn Gigg <martyn.gigg@gmail.com>
2017-08-11 17:22:24 +0100

    Add a file for global initialization of unit test suite.

--- python/matplotlib/cppwidget/widgets/mplcpp/test/PythonObjectTest.h
+++ python/matplotlib/cppwidget/widgets/mplcpp/test/PythonObjectTest.h
@@ -2,35 +2,10 @@
 #define PYTHONOBJECTTEST_H
 
 #include "cxxtest/TestSuite.h"
-#include "cxxtest/GlobalFixture.h"
 
 #include "PythonObject.h"
 #include "PythonErrors.h"
 
-/**
- * The cxxtest code ensures that the setup/tearDownWorld methods
- * are called exactly once per test-process. We use this
- * to initialize/shutdown the python interpreter
- */
-class PythonProcessHandler : CxxTest::GlobalFixture {
-public:
-  bool setUpWorld() override {
-    Py_Initialize();
-    return Py_IsInitialized();
-  }
-
-  bool tearDownWorld() override {
-    Py_Finalize();
-    return true;
-  }
-};
-
-// From the cxxtest manual:
-//
-// We can rely on this file being included exactly once
-// and declare this global variable in the header file.
-//
-static PythonProcessHandler pythonProcessHandler;
 
 class PythonObjectTest : public CxxTest::TestSuite {
 public:
