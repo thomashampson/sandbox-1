9a5a26e8d48bcf9ff5bdbb09e87fcdd8f9934afc
Martyn Gigg <martyn.gigg@gmail.com>
2018-09-28 11:02:20 +0100

    Improve colorbar example to update color limits

--- python/matplotlib/colorbar/cbar.py
+++ python/matplotlib/colorbar/cbar.py
@@ -2,12 +2,11 @@
 from __future__ import print_function
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas)
 from matplotlib.cm import ScalarMappable, get_cmap
-from matplotlib.colorbar import Colorbar
 from matplotlib.colors import Normalize
 from matplotlib.figure import Figure
 
@@ -21,21 +20,36 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle('ColorBar Example')
+        self.setCentralWidget(ColorbarWidget())
+
+
+class ColorbarWidget(QWidget):
+
+    def __init__(self, parent=None):
+        super(ColorbarWidget, self).__init__(parent)
         fig = Figure()
         rect = 0.25, 0.05, 0.1, 0.90
         self.cb_axes = fig.add_axes(rect)
-        self.setCentralWidget(FigureCanvas(fig))
+        self.canvas = FigureCanvas(fig)
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.canvas)
+        self.button = QPushButton("Update")
+        self.layout().addWidget(self.button)
+        self.button.pressed.connect(self._update_cb_scale)
 
-        self._create_colorbar()
+        self._create_colorbar(fig)
 
-    def _create_colorbar(self):
+    def _create_colorbar(self, fig):
         self.mappable = ScalarMappable(norm=Normalize(vmin=-1., vmax=1.),
-                                       cmap=DEFAULT_CMAP)
-        # The array is required but does not affect the output:
-        # https://stackoverflow.com/questions/28801803/matplotlib-scalarmappable-why-need-to-set-array-if-norm-set
+                                  cmap=DEFAULT_CMAP)
         self.mappable.set_array([])
-        self.colobar = Colorbar(ax=self.cb_axes,
-                                mappable=self.mappable)
+        self.cb = fig.colorbar(self.mappable, ax=self.cb_axes, cax=self.cb_axes)
+
+    def _update_cb_scale(self):
+        self.mappable.set_clim(30, 4300)
+        self.canvas.draw()
+
+
 
 
 if __name__ == '__main__':
