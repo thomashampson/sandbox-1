2d5fd573726f831d82434dc528df14dbe78b77d2
Martyn Gigg <martyn.gigg@gmail.com>
2020-06-11 18:35:56 +0100

    Use angle to define nonortho skew

--- python/matplotlib/nonorthognal/nonorthogonal_axes.py
+++ python/matplotlib/nonorthognal/nonorthogonal_axes.py
@@ -7,13 +7,14 @@ from mpl_toolkits.axisartist import Subplot
 
 
 def curvelinear_test1(fig):
+    angle = np.radians(45.)
     def tr(x, y):
         x, y = np.asarray(x), np.asarray(y)
-        return x, y - x
+        return x + np.cos(angle) * y, np.sin(angle) * y
 
     def inv_tr(x, y):
         x, y = np.asarray(x), np.asarray(y)
-        return x, y + x
+        return x - y / np.tan(angle), y / np.sin(angle)
 
     grid_helper = GridHelperCurveLinear((tr, inv_tr))
     ax1 = Subplot(fig, 1, 1, 1, grid_helper=grid_helper)
@@ -25,6 +26,8 @@ def curvelinear_test1(fig):
     ax1.set_aspect(1.)
     ax1.set_xlim(-10, 10.)
     ax1.set_ylim(-10, 10.)
+
+    print(ax1.get_xlim())
 
     ax1.grid(True)
 
