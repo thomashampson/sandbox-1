c89b14717bf68fe4f53a88c0d1345e7f6520a7f7
Martyn Gigg <martyn.gigg@gmail.com>
2020-09-26 18:54:41 +0100

    Add Qt example running async signal call
    Uses QtTest as a testing framework

---
+++ cpp/qt/process-events/build/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+
+file(REMOVE_RECURSE
+  "CMakeFiles/processEventsTest_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/processEventsTest_autogen.dir/ParseCache.txt"
+  "CMakeFiles/processEvents_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/processEvents_autogen.dir/ParseCache.txt"
+  "processEventsTest_autogen"
+  "processEvents_autogen"
+)
--- cpp/qt/process-events/test_mainwindow.cpp
+++ cpp/qt/process-events/test_mainwindow.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "test_mainwindow.h"
 #include "mainwindow.h"
 
@@ -10,3 +11,17 @@ void TestMainWindow::run() {
 
 QTEST_MAIN(TestMainWindow)
 //#include "testqstring.moc"
+||||||| constructed merge base
+=======
+#include "test_mainwindow.h"
+#include "mainwindow.h"
+
+#include <QtTest/QtTest>
+
+void TestMainWindow::run() {
+  MainWindow mainwindow;
+  QTest::mouseClick(mainwindow.centralWidget(), Qt::LeftButton);
+}
+
+QTEST_MAIN(TestMainWindow)
+>>>>>>> Add Qt example running async signal call
